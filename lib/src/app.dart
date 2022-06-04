ef156d372e757060d86a9807d65061c0cbc87fa0
hbedford <36144175+hbedford@users.noreply.github.com>
2022-06-04 14:07:45 -0300

    organization of project

---
+++ lib/src/app.dart
@@ -0,0 +1,16 @@
+import 'package:figmatokenization/src/screens/home_screen.dart';
+import 'package:flutter/material.dart';
+
+class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      initialRoute: '/home',
+      routes: {
+        '/home': (context) => const HomeScreen(),
+      },
+    );
+  }
+}
